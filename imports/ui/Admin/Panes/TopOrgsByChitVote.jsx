fa636577443de3962bd2924ca0760a772d69f63a
Avram Walden <avram@thebatterysf.com>
2018-11-07 17:36:02 -0800

    Add number input to Chit Voting Pane

--- imports/ui/Admin/Panes/TopOrgsByChitVote.jsx
+++ imports/ui/Admin/Panes/TopOrgsByChitVote.jsx
@@ -4,16 +4,26 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Organizations } from '/imports/api';
 import { ThemeMethods } from '/imports/api/methods';
 
-import { Loader, Grid, Table, Checkbox, Icon } from 'semantic-ui-react';
+import { Loader, Grid, Table, Checkbox, Icon, Input } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 import _ from 'underscore';
+
+const NumTopOrgsInput = styled(Input)`
+	width: 65px;
+
+	&& input {
+		padding: 0.3em 0.4em;
+	}
+`;
 
 export default class TopOrgsByChitVote extends React.Component {
 	constructor(props) {
 		super(props);
 
 		var preState = {
-			numberOfTopOrgs: Math.max(this.props.theme.numTopOrgs, this.props.theme.topOrgsManual.length)
+			numTopOrgs: this.props.theme.numTopOrgs,
+			calcNumTopOrgs: Math.max(this.props.theme.numTopOrgs, this.props.theme.topOrgsManual.length)
 		};
 
 		// Add each manually "Top Org" to state
@@ -25,6 +35,17 @@ export default class TopOrgsByChitVote extends React.Component {
 
 		this.topOrgToggle = this.topOrgToggle.bind(this);
 		this.sortTopOrgs = this.sortTopOrgs.bind(this);
+		this.updateNumTopOrgs = this.updateNumTopOrgs.bind(this);
+	}
+
+	updateNumTopOrgs(e, data) {
+		if(data.value !== this.props.theme.numTopOrgs){
+			ThemeMethods.update.call({id: this.props.theme._id, data: {numTopOrgs: data.value}});
+			this.setState({
+				numTopOrgs: data.value,
+				calcNumTopOrgs: Math.max(data.value, this.props.theme.topOrgsManual.length)
+			});
+		}
 	}
 
 	/**
@@ -32,17 +53,14 @@ export default class TopOrgsByChitVote extends React.Component {
 	 */
 	topOrgToggle(e, data){
 		ThemeMethods.topOrgToggle.call({theme_id: this.props.theme._id, org_id: data.org}, () => {
-			var newState = {numberOfTopOrgs: Math.max(this.props.theme.numTopOrgs, this.props.theme.topOrgsManual.length)};
+			var newState = {calcNumTopOrgs: Math.max(this.props.theme.numTopOrgs, this.props.theme.topOrgsManual.length)};
 			if(this.state[data.org]){
 				newState[data.org] = false;
 			} else {
 				newState[data.org] = true;
 			}
-			console.log(newState);
 			this.setState(newState);
 		});
-
-
 	}
 
 	render() {
@@ -51,7 +69,7 @@ export default class TopOrgsByChitVote extends React.Component {
 		return (
 			<React.Fragment>
 
-				<h1>Top Organizations</h1>
+				<h1>Top <NumTopOrgsInput size='mini' type='number' value={this.state.numTopOrgs} onChange={this.updateNumTopOrgs} width={1} /> Organizations</h1>
 
 				<Table celled>
 					<Table.Header>
@@ -64,7 +82,7 @@ export default class TopOrgsByChitVote extends React.Component {
 
 					<Table.Body>
 					{orgs.map((org, i) => {
-						let positive = i < this.state.numberOfTopOrgs;
+						let positive = i < this.state.calcNumTopOrgs;
 						return(
 							<Table.Row positive={positive} key={i}>
 								<Table.Cell>{org.title}</Table.Cell>
@@ -93,9 +111,9 @@ export default class TopOrgsByChitVote extends React.Component {
 		});
 
 		//Then bubble up the manual top orgs
-		if(this.state.numberOfTopOrgs > this.props.theme.topOrgsManual.length){
-			for(let i = sortedOrgs.length-1; i >= this.state.numberOfTopOrgs; i--){
-				console.log({i: i, num: this.state.numberOfTopOrgs});
+		if(this.state.calcNumTopOrgs > this.props.theme.topOrgsManual.length){
+			for(let i = sortedOrgs.length-1; i >= this.state.calcNumTopOrgs; i--){
+				console.log({i: i, num: this.state.calcNumTopOrgs});
 				// console.log({id: sortedOrgs[i]._id, index: i});
 				if(this.state[sortedOrgs[i]._id]){
 					// Find next auto top org
