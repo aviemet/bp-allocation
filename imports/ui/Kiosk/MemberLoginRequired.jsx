24dedc14afa53b2b37e31a088205315bfb666333
Avram Walden <avram@thebatterysf.com>
2019-06-10 12:59:01 -0700

    Updates kiosk UI

--- imports/ui/Kiosk/MemberLoginRequired.jsx
+++ imports/ui/Kiosk/MemberLoginRequired.jsx
@@ -11,9 +11,11 @@ import { VotingContextProvider } from './VotingContext';
 
 import MemberSearch from '/imports/ui/Components/MemberSearch';
 
+import { COLORS } from '/imports/global';
+
 const MemberLoginContainer = styled(Container)`
-  text-align: center;
-  padding-top: 6rem;
+	text-align: center;
+	padding-top: 6rem;
 
 	h1.title {
 		color: #FFF;
@@ -31,9 +33,32 @@ const MemberLoginContainer = styled(Container)`
 	}
 `;
 
+const Centered = styled.div`
+	position: absolute;
+	top: 50%;
+	transform: translateY(-50%);
+	z-index: 1000;
+`
+const BackgroundImage = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100vh;
+	opacity: 0.1;
+	z-index: 1;
+	background: url('/img/BPLogo.svg') no-repeat 50% 50%;
+	background-size: 1600px;
+`
+
 const MemberLoginRequired = props => {
 
-	const [ searchInput, setSearchInput ] = useState('');
+	const formRef = React.createRef();
+
+	// const [ searchInput, setSearchInput ] = useState('');
+	const [ initials, setInitials ] = useState('');
+	const [ number, setNumber ] = useState('');
+
 	const [ searchError, setSearchError ] = useState(false);
 	const [ user, setUser ] = useState(false);
 	const [ confirmUser, setConfirmUser ] = useState(false);
@@ -58,10 +83,16 @@ const MemberLoginRequired = props => {
 		}, 5000);
 	}
 
-	const chooseMember = () => {
+	const chooseMember = e => {
+		e.preventDefault();
+
 		setSearchError(false);
-		const member = _.find(members, ['code', searchInput.trim().toUpperCase()]);
-		setSearchInput('');
+		const code = `${initials.trim().toUpperCase()}${number}`;
+		// const member = _.find(members, ['code', searchInput.trim().toUpperCase()]);
+		const member = _.find(members, ['code', code]);
+		// setSearchInput('');
+		setInitials('');
+		setNumber('');
 		if(member) {
 			setUser(member);
 		} else {
@@ -69,6 +100,15 @@ const MemberLoginRequired = props => {
 		}
 	}
 
+	const SubmitButton = styled(Button)`
+		width: 100%;
+		text-align: center;
+		background-color: ${COLORS.blue} !important;
+		color: white !important;
+		border: 2px solid #fff !important;
+		font-size: 2rem !important;
+		text-transform: uppercase !important;
+	`
 	/*const resetMember = () => {
 		setConfirmUser(false);
 		setSearchInput('');
@@ -81,15 +121,18 @@ const MemberLoginRequired = props => {
 	}*/
 
 	const ChildComponent = props.component;
+	const submitDisabled = initials === '' || number === ''
 
 	// Display the interface to choose a member
 	if(!user) {
 		return(
 			<MemberLoginContainer>
-				<Header as='h1' className='title'>Enter Your Member ID</Header>
-					<Container style={{width: "80%"}}>
-						<Form onSubmit={chooseMember}>
-							<Form.Input fluid
+				<BackgroundImage />
+				<Centered>
+				<Header as='h1' className='title'>Enter Your Initials & Member ID</Header>
+					<Container>
+						<Form onSubmit={chooseMember} ref={formRef}>
+							{/*<Form.Input fluid
 								value={searchInput}
 								onChange={e => setSearchInput(e.target.value.trim().toUpperCase())}
 								type="text"
@@ -98,7 +141,31 @@ const MemberLoginRequired = props => {
 								iconPosition="left"
 								placeholder="Example: MB1234"
 								action={<Button>Search</Button>}
-							/>
+							/>*/}
+							<Form.Group inline>
+								<Form.Field>
+									<Input
+										style={{width: '325px'}}
+										size='huge'
+										label='Initials'
+										placeholder='Ex: MB'
+										value={initials}
+										onChange={e => setInitials(e.target.value.trim().toUpperCase())}
+									/>
+								</Form.Field>
+								<Form.Field>
+								<Input
+									size='huge'
+									label='Member #'
+									placeholder='Ex: 1234'
+									value={number}
+									onChange={e => setNumber(parseInt(e.target.value.trim()) || '')}
+								/>
+								</Form.Field>
+							</Form.Group>
+							<Form.Group>
+								<SubmitButton size='huge' disabled={submitDisabled} onClick={formRef.submit}>Begin Voting!</SubmitButton>
+							</Form.Group>
 						</Form>
 					</Container>
 					{/*<MemberSearch
@@ -116,6 +183,7 @@ const MemberLoginRequired = props => {
 				</React.Fragment>*/}
 
 				{searchError && <Header as='h2' className='title'>No Member Found, Try Again</Header>}
+				</Centered>
 			</MemberLoginContainer>
 		);
 	}
