228446f268b9e41f3de21b31f4f74a1e0e935367
Avram Walden <avram@thebatterysf.com>
2019-07-09 09:56:35 -0700

    Changed test structure back to how Meteor likes it

--- test/imports/ui/Presentation/Pages/index.test.js
+++
@@ -1,8 +0,0 @@
-import Intro from './Intro';
-import Orgs from './Orgs';
-import Timer from './Timer';
-import TopOrgs from './TopOrgs';
-import Allocation from './Allocation';
-import Results from './Results';
-
-export { Intro, Orgs, Timer, TopOrgs, Allocation, Results };
