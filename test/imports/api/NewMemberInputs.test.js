0dc1a774b09049790e047b623945fa07832f7140
Avram Walden <avram@thebatterysf.com>
2019-07-08 17:35:21 -0700

    Meteor doesn't run tests located in a folder called 'tests', how fucking intuitive! There's a few days of my life gone trying to figure that one out

--- test/imports/api/NewMemberInputs.test.js
+++ test/imports/api/NewMemberInputs.test.js
@@ -1,4 +1,4 @@
-import assert from 'assert';
+import { assert } from 'chai';
 
 import { Members, MemberThemes, Themes } from '/imports/api';
 import { ThemeMethods, MemberMethods } from '/imports/api/methods';
