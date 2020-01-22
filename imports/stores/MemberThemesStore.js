f345fd5c678fb919eedbdb25a40ebd1f6c52848d
Avram Walden <avram@thebatterysf.com>
2020-01-22 10:52:49 -0800

    Removed duplicate file

--- imports/stores/MemberThemesStore.js
+++
@@ -1,8 +0,0 @@
-import TrackableCollection from './TrackableCollection';
-import { observable, computed, action } from 'mobx';
-
-class MemberThemesStore extends TrackableCollection {
-	@observable working = true;
-}
-
-export default MemberThemesStore;
