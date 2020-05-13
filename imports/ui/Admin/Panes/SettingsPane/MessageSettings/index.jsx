3cdaae1959b6c0c149be7b92afecd1e33f82edd7
Avram Walden <aviemet@gmail.com>
2020-05-13 16:56:41 -0700

    Creates a rich text editor component

---
+++ imports/ui/Admin/Panes/SettingsPane/MessageSettings/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RichTextEditor from '/imports/ui/Components/RichTextEditor';
+
+const Messages = props => {
+	return (
+		<RichTextEditor />
+	);
+};
+
+export default Messages;
---
+++ imports/ui/Components/RichTextEditor/index.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const config = {};
+
+/* 
+ * Quill modules to attach to editor
+ * See https://quilljs.com/docs/modules/ for complete options
+ */
+config.modules = {
+	toolbar: [
+		[ 
+			{ 'header': '1' }, 
+			{ 'header': '2' }, 
+			{ 'header': '3' }, 
+			{ 'font': [] } 
+		],
+		[ { size: [] } ],
+		['bold', 'italic', 'underline', 'strike', 'blockquote'],
+		[ 
+			{ 'list': 'ordered' }, 
+			{ 'list': 'bullet' }, 
+			{ 'indent': '-1' }, 
+			{ 'indent': '+1' }
+		],
+		['link', 'image'],
+		['clean']
+	],
+	clipboard: {
+		// toggle to add extra line breaks when pasting HTML:
+		matchVisual: true,
+	}
+};
+
+/* 
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+config.formats = [
+	'header', 'font', 'size',
+	'bold', 'italic', 'underline', 'strike', 'blockquote',
+	'list', 'bullet', 'indent',
+	'link', 'image', 'video'
+];
+
+/* 
+ * Simple editor component that takes placeholder text as a prop 
+ */
+const Editor = props => {
+	const [ editorHtml, setEditorHtml ] = useState(props.content || '');
+
+	const handleChange = (content, delta) => {
+		setEditorHtml(content);
+		if(props.onChange) props.onChange(content);
+	};
+	
+	return (
+		<div>
+			<ReactQuill 
+				theme='snow'
+				onChange={ handleChange }
+				value={ editorHtml }
+				modules={ config.modules }
+				formats={ config.formats }
+				bounds={ '.app' }
+				placeholder={ props.placeholder || '' }
+			/>
+		</div>
+	);
+};
+
+/* 
+ * PropType validation
+ */
+Editor.propTypes = {
+	placeholder: PropTypes.string,
+	content: PropTypes.string,
+	onChange: PropTypes.any
+};
+
+export default Editor;
