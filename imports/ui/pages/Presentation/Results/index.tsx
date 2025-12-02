27a2176fca4ac199a56f03ac217f4fecb010a70e
Avram Walden <aviemet@gmail.com>
2025-12-01 18:46:20 -0800

    feat: reformats the results page

--- imports/ui/pages/Presentation/Results/index.tsx
+++ imports/ui/pages/Presentation/Results/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography } from "@mui/material"
+import { Box, Container, Typography } from "@mui/material"
 import { styled } from "@mui/system"
 import { cloneDeep } from "lodash"
 import numeral from "numeral"
@@ -11,69 +11,87 @@ import { Loading } from "/imports/ui/components"
 const Results = () => {
 	const { theme } = useTheme()
 	const { settings } = useSettings()
-	const { topOrgs } = useOrgs()
+	const { orgs, topOrgs } = useOrgs()
 
 	if(!theme) return <Loading />
 
-	const awardees: OrgDataWithComputed[] = []
-	const others: OrgDataWithComputed[] = []
+	const topOrgIds = new Set(topOrgs.map(org => org._id))
+	const restOrgs = orgs.filter(org => !topOrgIds.has(org._id))
+
 	const saves = theme?.saves?.reduce((sum, save) => sum + (save?.amount || 0), 0) || 0
 	let total = (theme?.leverageTotal || 0) + saves + (settings?.resultsOffset || 0)
 
-	cloneDeep(topOrgs).map((org) => {
+	cloneDeep(orgs).forEach((org) => {
 		total += (org?.pledgeTotal || 0) / 2
-
-		if(org.allocatedFunds + org.leverageFunds >= org.ask) {
-			awardees.push(org)
-		} else {
-			others.push(org)
-		}
-
-		return org
 	})
+
+	const getAwardType = (org: OrgDataWithComputed): "awardee" | "other" => {
+		const totalFunds = (org.allocatedFunds || 0) + (org.leverageFunds || 0)
+		return totalFunds >= (org.ask || 0) ? "awardee" : "other"
+	}
+
+	const getRunnerUpAmount = (org: OrgDataWithComputed): number => {
+		const baseAmount = (org.allocatedFunds || 0) + (org.leverageFunds || 0)
+		const consolation = theme.consolationActive ? (theme.consolationAmount || 0) : 0
+		return baseAmount + consolation
+	}
 
 	// let awardeesColumns = awardees.length > 3 ? parseInt(awardees.length / 2) + awardees.length % 2 : false
 
 	return (
 		<ResultsPageContainer maxWidth="xl" sx={ { p: 2 } }>
-			<AwardsImage><img src="/img/BAT_awards.png" alt="Battery Powered Logo" /></AwardsImage>
+			<HeaderSection>
+				<AwardsImage><img src="/img/BAT_awards.png" alt="Battery Powered Logo" /></AwardsImage>
+				<Typography component="h1" variant="h2" sx={ { marginBottom: 2 } }>Total amount given: { numeral(total).format("$0.[00]a") }</Typography>
+			</HeaderSection>
 
-			<Typography component="h1" variant="h2" sx={ { marginBottom: 2 } }>Total amount given: { numeral(total).format("$0.[00]a") }</Typography>
+			<AwardsSection>
+				<div>
+					<Typography component="h2" variant="h3" sx={ { marginBottom: 2 } }>Finalists</Typography>
+					<AwardsCards>
+						{ topOrgs.map((org) => {
+							return (
+								<AwardCard
+									key={ org._id }
+									org={ org }
+									award={ getAwardType(org) }
+									amount={ (org?.allocatedFunds || 0) + org.leverageFunds }
+								/>
+							)
+						}) }
+					</AwardsCards>
+				</div>
 
-			<AwardsCards>
-				{ awardees.map((org) => {
-					return (
-						<AwardCard
-							key={ org._id }
-							org={ org }
-							award={ "awardee" }
-							amount={ (org?.allocatedFunds || 0) + org.leverageFunds }
-						/>
-					)
-				}) }
-			</AwardsCards>
-
-			<br/>
-
-			<AwardsCards>
-				{ others.map((org) => {
-					return (
-						<AwardCard
-							key={ org._id }
-							org={ org }
-							award={ "other" }
-							amount={ (org?.allocatedFunds || 0) + org.leverageFunds }
-						/>
-					)
-				}) }
-			</AwardsCards>
-
+				<Box mt={ 6 }>
+					{ restOrgs.length > 0 && <>
+						<Typography component="h2" variant="h3" sx={ { marginBottom: 2 } }>Runners Up</Typography>
+						<AwardsCards>
+							{ restOrgs.map((org) => {
+								return (
+									<AwardCard
+										key={ org._id }
+										org={ org }
+										award={ getAwardType(org) }
+										amount={ getRunnerUpAmount(org) }
+										small={ true }
+									/>
+								)
+							}) }
+						</AwardsCards>
+					</> }
+				</Box>
+			</AwardsSection>
 		</ResultsPageContainer>
 	)
 }
 
 const ResultsPageContainer = styled(Container)( ({ theme }) => ({
 	color: "#FFF",
+	display: "grid",
+	gridTemplateRows: "auto 1fr",
+	minHeight: "100vh",
+	height: "100%",
+	gap: "2rem",
 
 	h1: {
 		fontSize: "3.6rem",
@@ -88,7 +106,6 @@ const ResultsPageContainer = styled(Container)( ({ theme }) => ({
 		},
 	},
 
-
 	".ui.cards .card .content": {
 		padding: "5px",
 
@@ -97,8 +114,21 @@ const ResultsPageContainer = styled(Container)( ({ theme }) => ({
 			fontSize: "1.75rem",
 		},
 	},
-	maxWidth: "lg",
+	maxWidth: "xl",
 }))
+
+const HeaderSection = styled("div")`
+	display: flex;
+	flex-direction: column;
+`
+
+const AwardsSection = styled("div")`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	justify-content: space-around;
+	padding-bottom: clamp(0px, 6vh, 160px);
+`
 
 const AwardsImage = styled("div")`
 	text-align: center;
